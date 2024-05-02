8337144f35d8662d26840cf41e3402f947cf1842
Igor Matozo <igor.matozo@live.com>
2024-05-02 15:29:37 -0300

    calculadora feita

---
+++ src/index.js
@@ -0,0 +1,23 @@
+// Função para adição
+function adicao(a, b) {
+    return a + b;
+}
+
+// Função para subtração
+function subtracao(a, b) {
+    return a - b;
+}
+
+// Função para multiplicação
+function multiplicacao(a, b) {
+    return a * b;
+}
+
+// Função para divisão
+function divisao(a, b) {
+    if (b === 0) {
+        return "Não é possível dividir por zero!";
+    } else {
+        return a / b;
+    }
+}
